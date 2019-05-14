d9743af588021dc9cf93ac1a3aeaa5b551c4ac32
matsruell <mats.ruell@student.kuleuven.be>
2019-05-14 14:35:01 +0200

    Update add-button.js

--- add-button.js
+++ add-button.js
@@ -10,7 +10,7 @@ AFRAME.registerComponent('add-button', {
             newElement.setAttribute('position', touchPoint)
             newElement.setAttribute('visible', 'false')
             newElement.setAttribute('scale', '0.0001 0.0001 0.0001')
-            newElement.setAttribute('gltf-model', '#sandCastleModel')
+            newElement.setAttribute('obj-model', '#sandCastleModel')
             this.el.sceneEl.appendChild(newElement)
             
             
