0850ec9ea713f6921d5717c952424176ce1c11b7
matsruell <mats.ruell@student.kuleuven.be>
2019-05-14 14:31:09 +0200

    Add files via upload

---
+++ add-button.js
@@ -0,0 +1,33 @@
+AFRAME.registerComponent('add-button', {
+    init: function() {
+        const addButton = document.getElementById('addbutton')
+        const ground = document.getElementById('ground')
+        const addObject = () => {
+            ground.addEventListener('click', event => {
+            const newElement = document.createElement('a-entity')
+            
+            const touchPoint = event.detail.intersection.point
+            newElement.setAttribute('position', touchPoint)
+            newElement.setAttribute('visible', 'false')
+            newElement.setAttribute('scale', '0.0001 0.0001 0.0001')
+            newElement.setAttribute('gltf-model', '#sandCastleModel')
+            this.el.sceneEl.appendChild(newElement)
+            
+            
+            newElement.addEventListener('model-loaded', () => {
+              // Once the model is loaded, we are ready to show it popping in using an animation
+              newElement.setAttribute('visible', 'true')
+              newElement.setAttribute('animation', {
+                property: 'scale',
+                to: '0.01 0.01 0.01',
+                easing: 'easeOutElastic',
+                dur: 800,
+              })
+            })
+          })
+        }
+                 
+        addButton.addEventListener('click',addObject);
+        // addButton.removeEventListener('click',addObject);
+    }
+ })
